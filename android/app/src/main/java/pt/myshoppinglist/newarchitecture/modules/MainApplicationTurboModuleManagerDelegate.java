a5bcb7ec1ae701495b86f423b22ecb39139f6aeb
João Sousa <joao.sousa3@vodafone.com>
2022-10-12 14:15:15 +0100

    feat: implement dev and prod firebase projects android

--- android/app/src/main/java/pt/myshoppinglist/newarchitecture/modules/MainApplicationTurboModuleManagerDelegate.java
+++ android/app/src/main/java/pt/myshoppinglist/newarchitecture/modules/MainApplicationTurboModuleManagerDelegate.java
@@ -1,4 +1,4 @@
-package com.myshoppinglist.newarchitecture.modules;
+package pt.myshoppinglist.newarchitecture.modules;
 
 import com.facebook.jni.HybridData;
 import com.facebook.react.ReactPackage;
